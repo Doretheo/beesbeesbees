954403bdd851e9c24eeced051f344b30b6638149
Theodore Nguyen <theodorenguyen5@gmail.com>
2020-08-25 16:46:06 -0700

    Completed Assignment

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  // using the call method allows me to call a function defined somewhere else and allowing access in the current context
+  // what parameter?
+  // I want the reference from this object
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color;
+  this.food;
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
